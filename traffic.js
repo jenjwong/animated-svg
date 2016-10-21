01283ea3636dba99048a22f331ab2b0805911433
Jen J Wong <jenjwong@gmail.com>
2016-10-20 23:08:32 -0700

    Refactors light change Styles svg

--- traffic.js
+++ traffic.js
@@ -1,61 +1,14 @@
-// $( document ).ready(function() {
-//   $( ".traffic-light path" ).not( $( ".contrast-content" ) ).css('fill', '#201F7C')
-//   const getColorInfo = (node) => {
-//     const lightInfo = {};
-//     lightInfo.node = node;
-//     lightInfo.color = node.id;
-//     lightInfo['color-on'] = lightInfo.color + '-on';
-//     return lightInfo;
-//   };
-//
-//   const lightOff = (node) => {
-//     const bulb = getColorInfo(node);
-//     node.removeClass(bulb['color-on']);
-//     node.addClass(bulb.color);
-//   };
-//
-//   const lightOn = (node) => {
-//     const bulb = getColorInfo(node);
-//     node.removeClass(bulb.color);
-//     const color = $(bulb['color-on'])
-//     node.addClass(color);
-//     console.log(node)
-//   };
-//
-//   const switchLights = (node) => {
-//     const $node = $(node)
-//     const isLastNode = $node.hasClass('last-node')
-//     const firstNode = document.querySelector('#bottom-front');
-//     var nextNode;
-//     if (isLastNode) {
-//       nextNode = firstNode
-//
-//     } else {
-//       nextNode = $node.prev();
-//     }
-//     lightOn($node);
-//     setTimeout(() => {
-//       lightOff($node);
-//       switchLights(nextNode);
-//     }, 2500);
-//   };
-//
-//   switchLights(document.querySelector('#bottom-front'));
-//
-// });
-//
-
-const rightStart = document.querySelector('#bottom-right')
-const frontStart = document.querySelector('#bottom-front')
-
+const rightStart = document.querySelector('#bottom-right');
+const frontStart = document.querySelector('#bottom-front');
 
 $( document ).ready(function() {
-  $( ".traffic-light path" ).not( $( ".contrast-content" ) ).css('fill', '#201F7C')
+
+  $(".traffic-light path").not($( ".contrast-content" )).css('fill', '#1B2A85');
   const getColorInfo = (node) => {
     const lightInfo = {};
     lightInfo.node = node;
     lightInfo.color = node.id;
-    lightInfo['color-on'] = lightInfo.color + '-on';
+    lightInfo['color-on'] = `${lightInfo.color}-on`;
     return lightInfo;
   };
 
@@ -71,31 +24,18 @@ $( document ).ready(function() {
     node.classList.add(bulb['color-on']);
   };
 
-  const switchLights = (node, firstNode) => {
+  const switchLights = (node, firstNode, time) => {
     const isLastNode = Array.from(node.classList).indexOf('last-node') > -1;
-    const isBulb = Array.from(node.classList).indexOf('bulb') > -1;
-    var nextNode;
-    if (isLastNode) {
-      nextNode = firstNode
-
-    } else {
-      nextNode = node.previousElementSibling
-    }
-    console.log(isBulb)
-    if (isBulb) {
-      lightOn(node);
-      setTimeout(() => {
-        lightOff(node);
-        switchLights(nextNode, firstNode);
-      }, 4000);
-    }
+    let nextNode;
+    isLastNode ? nextNode = firstNode : nextNode = node.previousElementSibling;
+    lightOn(node);
+    setTimeout(() => {
+      lightOff(node);
+      switchLights(nextNode, firstNode);
+    }, 2500);
   };
 
-  console.log(document.querySelector('#bottom-right'))
-
-
-
   switchLights(document.querySelector('#bottom-front'), frontStart);
-  // switchLights(document.querySelector('#bottom-right'), rightStart);
+  switchLights(document.querySelector('#top-right'), rightStart);
 
 });
