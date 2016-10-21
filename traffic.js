51eecb64686a0333ddf57e854776133c61662a51
Jen Wong <jenjwong@gmail.com>
2016-10-20 23:22:52 -0700

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -3,7 +3,7 @@ const frontStart = document.querySelector('#bottom-front');
 
 $( document ).ready(function() {
 
-  $(".traffic-light path").not($( ".contrast-content" )).css('fill', '#201F7C');
+  $(".traffic-light path").not($( ".contrast-content" )).css('fill', '#074177');
   const getColorInfo = (node) => {
     const lightInfo = {};
     lightInfo.node = node;
